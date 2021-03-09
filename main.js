9ddad9bff8fb2452fc3b41a6c37643b4e1dc7023
giuseppe <placidagiuseppe@gmail.com>
2021-03-09 15:36:54 +0100

    ....

---
+++ main.js
@@ -0,0 +1,26 @@
+//computer deve generare 16 numeri casuali compresi tra 1 a 100
+var arraybomb= [];
+var max = 100;
+creaBomba(arraybomb, max);
+console.log(arraybomb);
+random(1,100);
+
+function creaBomba(array, max){
+while(array.length < 16) {
+  var numeri = random(1, max);
+  if (!array.includes(numeri)) {
+    array.push();
+  }
+}
+return array;
+}
+
+//funzione  genera numeri casuali
+
+function random(min , max){
+  if (isNaN(min) || (max)) {
+    console.log("Non sono numeri");
+  }else {
+    return Math.floor(Math.random()*(max-min +1)+1);
+  }
+}
